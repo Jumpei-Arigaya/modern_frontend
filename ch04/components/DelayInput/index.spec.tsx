577c56ac3d4023748fb85330c912cde096c3420c
Jumpei-Arigaya <junpeeei0407@gmail.com>
2023-08-14 11:59:11 +0900

    ユニットテスト

--- ch04/components/DelayInput/index.spec.tsx
+++ ch04/components/DelayInput/index.spec.tsx
@@ -6,6 +6,7 @@ import {
   act,
 } from "@testing-library/react";
 import { DelayInput } from "./index";
+import { HTMLInputTypeAttribute } from "react";
 
 // DelayInputコンポーネントに関するテスト
 describe("DelayInput", () => {
@@ -23,12 +24,46 @@ describe("DelayInput", () => {
     jest.useFakeTimers();
   });
 
-  afterEach(() => {
+  afterEach(async () => {
     // テスト中に使用したjestのタイマーをリセットします
     // 現在pending中のタイマーのみ実行する
-    jest.runOnlyPendingTimers();
+    await act(() => {
+      jest.runOnlyPendingTimers();
+    });
 
     // タイマーを元のものに戻す
     jest.useRealTimers();
+
+    renderResult.unmount();
+  });
+
+  // span要素のテキストが空であることをテスト
+  it("should displau empty in span on initial render", () => {
+    const spanNode = screen.getByTestId("display-text") as HTMLSpanElement;
+
+    // 初期表示は空
+    expect(spanNode).toHaveTextContent("入力したテキスト:");
+  });
+
+  // 入力して1秒後にテキストが表示されるかテスト
+  it("should display input text 1second after onChange event occurs", async () => {
+    const inputText = "Test Input Text";
+    const inputNode = screen.getByTestId("input-text") as HTMLInputElement;
+
+    // inputのonChangeイベントを呼び出す
+    fireEvent.change(inputNode, { target: { value: inputText } });
+
+    // act関数内で実行することにより、タイマーのコールバック中で起きる状態変更が反映されることを保証する
+    await act(() => {
+      // タイマーにセットされたtimeoutをすべて実行する
+      jest.runAllTimers();
+    });
+    const spanNode = screen.getByTestId("display-text") as HTMLSpanElement;
+
+    // 入力したテキストが表示されるか確認
+    expect(spanNode).toHaveTextContent(`入力したテキスト: ${inputText}`);
+
+    // モック関数を渡し、呼ばれたか確認する
+    expect(handleChange).toHaveBeenCalled();
   });
 });
